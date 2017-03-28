26af2e022625833612d08949000738c25b6b51a1
jalejos <jalejos@nearsoft.com>
2017-03-28 09:50:05 -0700

    Add details view with navigation controller

---
+++ CatalogApplication/ViewControllers/MediaDetailsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MediaDetailsViewController.swift
+//  CatalogApplication
+//
+//  Created by Alejos on 3/28/17.
+//  Copyright © 2017 Alejos. All rights reserved.
+//
+
+import UIKit
+
+class MediaDetailsViewController: UIViewController {
+
+    @IBOutlet weak var previewImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var directorLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var hyperlinkImageView: UIImageView!
+    @IBOutlet weak var hyperlinkButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
