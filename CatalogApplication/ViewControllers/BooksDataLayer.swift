9d707c9ae2228bfb4a2402dbbe803c93698f1038
jalejos <jalejos@nearsoft.com>
2017-03-28 12:28:57 -0700

    Convert book json into array

---
+++ CatalogApplication/ViewControllers/BooksDataLayer.swift
@@ -0,0 +1,30 @@
+//
+//  BooksDataLayer.swift
+//  CatalogApplication
+//
+//  Created by Alejos on 3/28/17.
+//  Copyright © 2017 Alejos. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+struct BooksDataLayer {
+    
+    static let wrongContentError = NSError.init(domain: "", code: 204, userInfo: nil)
+    
+    static func getMovies(from offset: Int, onComplete: @escaping (_ books: [Book]?, _ error: Error?) -> Void) {
+        BooksService.getBooks(from: offset) { (booksJSON, error) in
+            if booksJSON != nil {
+                guard let arrayJSON = booksJSON!["results"] as? Array<Dictionary<String, Any>> else {
+                    onComplete(nil, wrongContentError);
+                    return
+                }
+                let booksArray = Mapper<Book>().mapArray(JSONArray: arrayJSON)
+                onComplete(booksArray, nil)
+            } else {
+                onComplete(nil, error)
+            }
+        }
+    }
+}
