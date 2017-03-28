1fba5da2bd39ffe45fb92f7b15f10dee5447aff5
jalejos <jalejos@nearsoft.com>
2017-03-28 16:04:28 -0700

    Display categories in table view

--- CatalogApplication/ViewControllers/CategoryTableViewCell.swift
+++ CatalogApplication/ViewControllers/CategoryTableViewCell.swift
@@ -13,15 +13,8 @@ class CategoryTableViewCell: UITableViewCell {
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var categoryLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configWithCategory (category: String) {
+        categoryLabel.text = category
     }
 
 }
--- CatalogApplication/ViewControllers/TopCategoriesTableViewController.swift
+++ CatalogApplication/ViewControllers/TopCategoriesTableViewController.swift
@@ -13,14 +13,21 @@ class TopCategoriesTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.register(UINib(nibName: "CategoryTableViewCell", bundle: nil), forCellReuseIdentifier: "CategoryCell")
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 80
     }
     
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return Categories.count
     }
 
-    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! CategoryTableViewCell
+        cell.configWithCategory(category: Categories.array[indexPath.row].rawValue)
+        return cell
+    }
 
 }
