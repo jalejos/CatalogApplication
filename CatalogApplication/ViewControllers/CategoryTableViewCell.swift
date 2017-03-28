ad639db80f803915dc4b7121eb8b66dacb1395cb
jalejos <jalejos@nearsoft.com>
2017-03-28 15:56:58 -0700

    Add category table view cell

---
+++ CatalogApplication/ViewControllers/CategoryTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryTableViewCell.swift
+//  CatalogApplication
+//
+//  Created by Alejos on 3/28/17.
+//  Copyright © 2017 Alejos. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
