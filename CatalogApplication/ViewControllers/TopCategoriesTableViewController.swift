c398ca699793ce570e9b9e81c75faf98dcea2cc0
jalejos <jalejos@nearsoft.com>
2017-03-28 15:54:15 -0700

    Add top categories table view on tab bar

---
+++ CatalogApplication/ViewControllers/TopCategoriesTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TopCategoriesTableViewController.swift
+//  CatalogApplication
+//
+//  Created by Alejos on 3/28/17.
+//  Copyright © 2017 Alejos. All rights reserved.
+//
+
+import UIKit
+
+class TopCategoriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    
+
+}
