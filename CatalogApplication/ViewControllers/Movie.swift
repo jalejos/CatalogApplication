1957696dc7ed326b969c2bb21d0adb1c3dd047c1
jalejos <jalejos@nearsoft.com>
2017-03-28 10:05:45 -0700

    Add more properties to movie model

--- CatalogApplication/ViewControllers/Movie.swift
+++ CatalogApplication/ViewControllers/Movie.swift
@@ -13,6 +13,10 @@ class Movie: Mappable {
     var title: String = ""
     var date: String = ""
     var imageURL: URL? = URL.init(string: "")
+    var director: String = ""
+    var rating: String = ""
+    var summary: String = ""
+    var articleURL: URL? = URL.init(string: "")
     
     let transformURL = TransformOf<URL, String>(fromJSON: { (value: String?) -> URL? in
         // transform value from String to URL
@@ -36,5 +40,9 @@ class Movie: Mappable {
         title <- (map["display_title"])
         date <- (map["publication_date"])
         imageURL <- (map["multimedia.src"], transformURL)
+        director <- (map["byline"])
+        rating <- (map["mpaa_rating"])
+        summary <- (map["summary_short"])
+        articleURL <- (map["link.url"], transformURL)
     }
 }
