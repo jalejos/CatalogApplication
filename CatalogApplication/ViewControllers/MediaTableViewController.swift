85252ca152a35c8fc249f55751f5636615603cae
jalejos <jalejos@nearsoft.com>
2017-03-30 10:36:34 -0700

    Add xib registration on base controller

--- CatalogApplication/ViewControllers/MediaTableViewController.swift
+++ CatalogApplication/ViewControllers/MediaTableViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class MediaTableViewController: UITableViewController {
 
-    let localNibName = "MediaTableViewCell"
-    let cellId = "MediaCell"
+    let objectNibName = "MediaTableViewCell"
+    let mediaCellId = "MediaCell"
+    let loadNibName = "LoadTableViewCell"
+    let loadCellId = "LoadCell"
     let estimatedCellHeight = CGFloat(70)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.register(UINib(nibName: localNibName, bundle: nil), forCellReuseIdentifier: cellId)
+        tableView.register(UINib(nibName: objectNibName, bundle: nil), forCellReuseIdentifier: mediaCellId)
+        tableView.register(UINib(nibName: loadNibName, bundle: nil), forCellReuseIdentifier: loadCellId)
         self.tableView.rowHeight = UITableViewAutomaticDimension
         self.tableView.estimatedRowHeight = estimatedCellHeight
     }
