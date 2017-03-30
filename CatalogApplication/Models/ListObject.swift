7fbbf6b6ff688b8c7fd8c50ab2843e356b564527
jalejos <jalejos@nearsoft.com>
2017-03-30 01:43:15 -0700

    Add transform date format method

--- CatalogApplication/Models/ListObject.swift
+++ CatalogApplication/Models/ListObject.swift
@@ -31,6 +31,32 @@ class ListObject: Mappable {
         return nil
     })
     
+    let transformDateFormat = TransformOf<String, String>(fromJSON: { (value: String?) -> String? in
+        // transform value from api format to app format
+        if let apiString = value {
+            let apiDateFormatter = DateFormatter()
+            apiDateFormatter.dateFormat = "y-MM-dd"
+            let date = apiDateFormatter.date(from: apiString)
+            let appDateFormatter = DateFormatter()
+            appDateFormatter.dateFormat = "MMMM dd, y"
+            let appString = appDateFormatter.string(from: date!)
+            return appString
+        }
+        return nil
+    }, toJSON: { (value: String?) -> String? in
+        // transform value from app format to api format
+        if let appString = value {
+            let appDateFormatter = DateFormatter()
+            appDateFormatter.dateFormat = "MMMM dd, y"
+            let date = appDateFormatter.date(from: appString)
+            let apiDateFormatter = DateFormatter()
+            apiDateFormatter.dateFormat = "y-MM-dd"
+            let apiString = apiDateFormatter.string(from: date!)
+            return apiString
+        }
+        return nil
+    })
+    
     convenience required init?(map: Map) {
         self.init()
     }
