b50f09c6cd8f40ae2619f133762dc26d421906ba
jalejos <jalejos@nearsoft.com>
2017-03-30 13:13:46 -0700

    Refactor book data layer

--- CatalogApplication/DataAccess/DataLayers/BooksDataLayer.swift
+++ CatalogApplication/DataAccess/DataLayers/BooksDataLayer.swift
@@ -8,19 +8,35 @@
 
 import Foundation
 import ObjectMapper
+import RealmSwift
 
 class BooksDataLayer: DataLayer {
     static func getBooks(from offset: Int, onComplete: @escaping (_ books: [Book]?, _ error: Error?) -> Void) {
-        BooksService.getBooks(from: offset) { (booksJSON, error) in
-            if booksJSON != nil {
-                guard let arrayJSON = booksJSON![jsonKey] as? Array<Dictionary<String, Any>> else {
-                    onComplete(nil, wrongContentError);
-                    return
+        let realm = try! Realm()
+        let books = realm.objects(Book.self)
+        if books.count > offset + offsetSize {
+            let booksArray = Array(books)
+            let displayBooksArray = Array(booksArray[offset..<offset+20])
+            onComplete(displayBooksArray, nil)
+        } else {
+            BooksService.getBooks(from: offset) { (booksJSON, error) in
+                if booksJSON != nil {
+                    guard let arrayJSON = booksJSON![jsonKey] as? Array<Dictionary<String, Any>> else {
+                        onComplete(nil, wrongContentError);
+                        return
+                    }
+                    let booksArray = Mapper<Book>().mapArray(JSONArray: arrayJSON)
+                    if let booksArray = booksArray {
+                        for book in booksArray {
+                            try! realm.write() {
+                                realm.add(book, update: true)
+                            }
+                        }
+                    }
+                    onComplete(booksArray, nil)
+                } else {
+                    onComplete(nil, error)
                 }
-                let booksArray = Mapper<Book>().mapArray(JSONArray: arrayJSON)
-                onComplete(booksArray, nil)
-            } else {
-                onComplete(nil, error)
             }
         }
     }
