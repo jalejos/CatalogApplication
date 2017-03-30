133720ebad8bd15b7f5c9b07e4a52e96fd1ad1c7
jalejos <jalejos@nearsoft.com>
2017-03-30 00:29:06 -0700

    Add base DataLayer class

---
+++ CatalogApplication/DataAccess/DataLayers/DataLayer.swift
@@ -0,0 +1,13 @@
+//
+//  DataLayer.swift
+//  CatalogApplication
+//
+//  Created by Alejos on 3/30/17.
+//  Copyright © 2017 Alejos. All rights reserved.
+//
+
+import Foundation
+
+class DataLayer {
+    static let wrongContentError = NSError.init(domain: "", code: 204, userInfo: nil)
+}
