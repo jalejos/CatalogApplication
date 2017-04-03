c97fea0121a0b4b885d366a227ddc181d1d6c1e9
jalejos <jalejos@nearsoft.com>
2017-04-03 12:54:45 -0600

    Refactor media table view controller

--- CatalogApplication/ViewControllers/MediaTableViewController.swift
+++ CatalogApplication/ViewControllers/MediaTableViewController.swift
@@ -15,14 +15,19 @@ class MediaTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
+    }
+    
+    func configureTableView() {
         tableView.register(UINib(nibName: MediaTableViewCell.reusableCellID(), bundle: nil), forCellReuseIdentifier: MediaTableViewCell.reusableCellID())
         tableView.register(UINib(nibName: reusableLoadCellID, bundle: nil), forCellReuseIdentifier: reusableLoadCellID)
         self.tableView.rowHeight = UITableViewAutomaticDimension
         self.tableView.estimatedRowHeight = estimatedCellHeight
     }
+}
 
-    // MARK: - Table view data source
-
+// MARK: - Table view data source
+extension MediaTableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -34,6 +39,4 @@ class MediaTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
-    
-
 }
