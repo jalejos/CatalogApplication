f0234f365935acea0f4ecac3684d27c8a7f2ee0e
jalejos <jalejos@nearsoft.com>
2017-04-03 12:47:15 -0600

    Add SegueHandler

---
+++ CatalogApplication/Utilities/SegueHandler.swift
@@ -0,0 +1,16 @@
+//
+//  SegueHandler.swift
+//  CatalogApplication
+//
+//  Created by Alejos on 4/3/17.
+//  Copyright © 2017 Alejos. All rights reserved.
+//
+
+enum SegueHandler: String {
+    case loginSegue
+    case logoutSegue
+    case bookSegue
+    case categorySegue
+    case movieSegue
+    case webSegue
+}
